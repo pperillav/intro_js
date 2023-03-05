f15b6f15ac7d785a3800639c5912210bc3ee2aaf
Pedro Perilla <pperillav@unal.edu.co>
2023-03-05 18:22:28 -0500

    Taller_1_Pedro_Perilla

---
+++ programa.js
@@ -0,0 +1,64 @@
+// Taller : Definir dos variables, asignarles un valor  y utilizando decisiones imprimir 
+// en la consola la que tenga menor valor
+
+// Taller: Definir 3 variables, asignarles un valor  y utilizando decisiones imprimir 
+// en la consola la que tenga el mayor valor
+
+let a = 5;
+let b = 10;
+
+
+if (a<b){
+    console.log("a tiene menor valor")
+}else{
+    console.log("a tiene menor valor")
+}
+
+let c = 15;
+let d = 20;
+let e = 25;
+
+if (c>d && c>e){
+    console.log("c es la de mayor valor")
+}else if (d>c && d>e){
+    console.log("d es la de mayor valor")
+}else{
+    console.log("e es la de mayor valor")
+}
+//No tipado: No define el tipo de dato que va a guardar en las variables
+//let a = 2;
+//let  b = 8;
+
+//console.log(a+b);
+
+//Aritméticas: * + - / %
+
+//Comparación: (condiciones)
+// > < >= <= == === !=
+
+//console.log(a===b)
+
+//Casting automático: conversión automática del tipo de dato
+
+//Operaciones lógicas: AND OR
+//a= true
+//b = false
+
+//console.log(a && b)
+//console.log(a || b)
+
+//Romper secuencialidad del programa para evitar líneas de código
+
+//Decisión 
+
+//if(condición){ }
+
+// if (a>b){
+//        console.log("a es mayor que b")
+//}
+
+//if (a>b){
+//    console.log("a es mayor que b")
+//}else{
+//    console.log("a es menor que b")
+//}
